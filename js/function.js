4cf9faf70d68d215420c60b0163cacea49823efa
wtoaabb <32261704+wtoaabb@users.noreply.github.com>
2024-03-12 10:45:39 +0800

    Add files via upload

---
+++ js/function.js
@@ -0,0 +1,10 @@
+function into(){
+	var imgcon="<img src=\"./img/start.jpg\" style=\"height: 100%;width: 100%;\" alt=\"\" />";
+	$("#startImg").html(imgcon);
+	$("#startinto").hide();
+}
+
+function cpstart(){
+	$("#startImg").hide();
+	$("#startcp").hide();
+}
